47738cb364038e225a72a851b7e17ba47bb7fbe1
celigx <l.celigx@gmail.com>
2023-03-02 09:27:27 +0100

    Fix className typo

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div class="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen">
       <div
-        class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status"
       >
-        <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
       </div>
     </div>
   );
