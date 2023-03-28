e5642530442bd2df5873479e6bb632d718d32ff8
celigx <l.celigx@gmail.com>
2023-03-28 19:56:46 +0200

    Update padding, margin, and max-width of navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center h-16 px-6 md:px-20 lg:max-w-screen-xl lg:mx-auto">
+    <nav className="flex items-center h-16 px-6 lg:px-12 lg:max-w-screen-2xl 2xl:mx-auto 2xl:px-0">
       <ul className="flex">
         <li className="mr-10">
           <Link to={"/"} className="text-md hover:text-gray-500 duration-150">
             Home
           </Link>
         </li>
-        <li className="mr-10">
+        <li className="mr-0">
           <Link to={"/projects"} className="text-md hover:text-gray-500 duration-150">
             Projects
           </Link>
