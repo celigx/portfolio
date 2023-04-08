c34d5b9d2a36c9272830721e0e2e49fd90fc750e
celigx <l.celigx@gmail.com>
2023-04-08 10:54:32 +0200

    Fix padding and max-width

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white ">
-      <div className="flex items-center justify-between h-16 px-6 md:px-20 lg:max-w-screen-xl lg:mx-auto">
+      <div className="flex items-center justify-between h-16 px-6 lg:px-12 lg:max-w-screen-2xl 2xl:mx-auto 2xl:px-0">
         <p className="">celigx</p>
         <a href="https://github.com/celigx" className="">
           github.com
